a5506ac57f57ffa636a608b170b6dc8d44fb7729
Davis Luke Brown <84476975+Davis-L-Brown@users.noreply.github.com>
2022-11-14 00:48:37 -0500

    added a collision function to alert a player when they lose. Added a readme for citations and description purposes

---
+++ DinoJump/README.md
@@ -0,0 +1,9 @@
+Author: Davis Brown	
+Date:	11/13/2022
+Title: Dino Jump
+
+A basic cube runner game, mimicking googles dinosaur cactus jump.
+
+Inspiration and the core structure for this game came from the following tutorial: 
+	"The Easiest Javascript Game Ever" by KnifeCircus on youtube
+	https://www.youtube.com/watch?v=bG2BmmYr9NQ&ab_channel=KnifeCircus
--- DinoJump/js/index.js
+++ DinoJump/js/index.js
@@ -1,5 +1,6 @@
 var character = document.getElementById("character");
 var block = document.getElementById("block");
+
 function jump() {
   if(character.classlist != "animate"){
     character.classList.add("animate")
@@ -7,3 +8,13 @@ function jump() {
   setTimeout(function(){
     character.classList.remove("animate");},500);
 }
+
+var checkDead = setInterval(function () {
+    var charTop = parseInt(window.getComputedStyle(character).getPropertyValue("top"));
+    var blockLeft = parseInt(window.getComputedStyle(block).getPropertyValue("left"));
+    if ((blockLeft < 20 && blockLeft > 0) && (charTop >= 130)) {
+        block.style.animation = "none";
+        block.style.display = "none";
+        alert("You Lose");
+    }
+}, 10);

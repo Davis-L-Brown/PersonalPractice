20348b2d9fbde46b5bea33fb7f4fd3e48c145a97
Davis Luke Brown <84476975+Davis-L-Brown@users.noreply.github.com>
2022-11-15 00:07:11 -0500

    added divs to create a scoreboard on in the html added styling to both the game, the scoreboard, and the game board as well as made the game board taller added a js function (window keypress event) to allow users to jump using the spacebar

--- DinoJump/js/index.js
+++ DinoJump/js/index.js
@@ -1,14 +1,5 @@
 var character = document.getElementById("character");
 var block = document.getElementById("block");
-
-function jump() {
-  if(character.classlist != "animate"){
-    character.classList.add("animate")
-  }
-  setTimeout(function(){
-    character.classList.remove("animate");},500);
-}
-
 var checkDead = setInterval(function () {
     var charTop = parseInt(window.getComputedStyle(character).getPropertyValue("top"));
     var blockLeft = parseInt(window.getComputedStyle(block).getPropertyValue("left"));
@@ -18,3 +9,18 @@ var checkDead = setInterval(function () {
         alert("You Lose");
     }
 }, 10);
+
+window.onkeypress = function (e) {
+    if (e.keyCode == 32) { jump(); }
+}
+
+function jump() {
+  if(character.classlist != "animate"){
+    character.classList.add("animate")
+  }
+  setTimeout(function(){
+    character.classList.remove("animate");},500);
+}
+
+
+

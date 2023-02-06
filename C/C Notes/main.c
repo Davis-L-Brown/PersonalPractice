dd3ed703dc6d58bc8d1938f975dac2d03ab351f9
Davis Brown <99browndl@gmail.com>
2023-02-05 21:44:14 -0500

    added some more nots

--- C/C Notes/main.c
+++ C/C Notes/main.c
@@ -16,7 +16,14 @@ int main() {
 	printf("Hello \bWorld!\n");
 	// \f form feed
 	printf("Hello World!");
-	// \r - return carriage (essentially deletes anything before \r and returns everything after
+	/* \r - return carriage
+		moves the cursor to the beginning of the current line horizontally
+		will print like so: 
+		0 1 2 3 4 5 
+		H e l l o		- cursor moves back to index 0 and prints remainder of string from there
+		W o r l d !
+		output: World!
+	 */
 	printf("Hello \r world!");
 	// \t - horizontal tab 4 spaces
 	printf("Hello \t world!");

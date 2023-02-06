2bb52b892380a916271257c825b91181cfb5e8c5
Davis Brown <99browndl@gmail.com>
2023-02-05 21:50:12 -0500

    added better example

--- C/C Notes/main.c
+++ C/C Notes/main.c
@@ -15,7 +15,7 @@ int main() {
 	// \b - backspace
 	printf("Hello \bWorld!\n");
 	// \f form feed
-	printf("Hello World!");
+	printf("Hello World!\n");
 	/* \r - return carriage
 		moves the cursor to the beginning of the current line horizontally
 		will print like so: 
@@ -29,5 +29,13 @@ int main() {
 	printf("Hello \t world!");
 	// \v - vertical tab
 	printf("Hello \v World!");
+
+	// combined example
+	printf("David says , \" Programming is fun !\"\n");
+	printf("**Conditions apply , \"Offers valid until tomorrow\"\n");
+	printf("C:\\My computer\\My folder\n");
+	printf("D:/My documents/My folder\n");
+	printf("\\\\\\\\Today is holiday\\\\\\\\\n");
+	printf("this is a triple quoted string\"\"\"This month has 30 days\"\"\"\n");
 	return 0;
 }

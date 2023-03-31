e1769805340cff0bca09d810261ea9820e41eb7a
Davis Brown <Davis.Brown@gentex.com>
2023-03-31 17:22:38 -0400

    added the same functionality to sidebar that was in menu bar - had the same issue where bar would populate the sidebar based on all items across every form using the sidebar (would put aboutForm menu items in settingFrom menu items and vice versa) for reference: https://stackoverflow.com/questions/64663743/duplicate-contextmenu-menuitems-in-observablecollection-dependancyproperty-in-u . Added an aboutForm and a SettingsForm

---
+++ C#/DataVisualization/DataVisualization/Views/UserControls/SideBar.xaml.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Markup;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace DataVisualization.Views.UserControls
+{
+    /// <summary>
+    /// Interaction logic for SideBar.xaml
+    /// </summary>
+    [ContentProperty("SideMenuItems")]
+    public partial class SideBar : UserControl
+    {
+
+        public ObservableCollection<DependencyObject> SideMenuItems
+        {
+            get { return (ObservableCollection<DependencyObject>)GetValue(SideMenuItemsProperty); }
+            set { SetValue(SideMenuItemsProperty, value); }
+        }
+
+        internal static readonly DependencyProperty SideMenuItemsProperty = DependencyProperty.Register(
+            nameof(SideMenuItems), 
+            typeof(ObservableCollection<DependencyObject>), 
+            typeof(SideBar));
+
+        public SideBar()
+        {
+            SetCurrentValue(SideMenuItemsProperty, new ObservableCollection<DependencyObject>());
+            InitializeComponent();
+        }
+
+        
+
+    }
+}

191d80211978a9f47e6d68abce0f66f90ef3c9ee
Davis Brown <99browndl@gmail.com>
2023-05-01 14:01:08 -0400

    using the Microsoft Excel Object Library v16, opened the excel application, create a workboox, sheet, and named the columns

--- CSharp/WPF Tutorial/WPF Tutorial/WPF Tutorial/Forms/Tutorial26.xaml.cs
+++ CSharp/WPF Tutorial/WPF Tutorial/WPF Tutorial/Forms/Tutorial26.xaml.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Reflection;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows;
@@ -12,6 +13,9 @@ using System.Windows.Media;
 using System.Windows.Media.Imaging;
 using System.Windows.Shapes;
 
+using Excel = Microsoft.Office.Interop.Excel;
+
+
 namespace WPF_Tutorial.Forms
 {
     /// <summary>
@@ -22,6 +26,34 @@ namespace WPF_Tutorial.Forms
         public Tutorial26()
         {
             InitializeComponent();
+        }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            Excel.Application oXL;
+            Excel.Workbook oWB;
+            Excel.Worksheet oSheet;
+            Excel.Range oRng;
+            try
+            {
+                /// start excel and get application object
+                oXL = new Excel.Application();
+                oXL.Visible = true;
+
+                /// get a new workbook
+                oWB = (Excel.Workbook)(oXL.Workbooks.Add(Missing.Value));
+                oSheet = (Excel.Worksheet)oWB.ActiveSheet;
+
+                /// add table headers going cell by cell
+                oSheet.Cells[1, 1] = "First Name";
+                oSheet.Cells[1, 2] = "Last Name";
+                oSheet.Cells[1, 3] = "Full Name";
+                oSheet.Cells[1, 4] = "Salary";
+            }
+            catch(Exception ex) 
+            {
+                
+            }
         }
     }
 }
